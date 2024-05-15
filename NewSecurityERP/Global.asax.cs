06b38c90628008646e5834e104855fef8bb2b468
Aakash <aakashRuhal99@gmail.com>
2024-05-15 15:47:21 +0530

    NewUserRegistration > Complete all Tabs UI

--- NewSecurityERP/Global.asax.cs
+++ NewSecurityERP/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
 
@@ -12,37 +13,9 @@ namespace NewSecurityERP
 
         protected void Application_Start(object sender, EventArgs e)
         {
-
-        }
-
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
+            RouteTable.Routes.MapPageRoute("", "", "~/Default.aspx");
+            RouteTable.Routes.MapPageRoute("Dashboard", "Dashboard", "~/Dashboard.aspx");
+            RouteTable.Routes.MapPageRoute("NewUserRegistration", "NewUserRegistration", "~/CandidateRegistration/NewUserRegistration.aspx");
         }
     }
 }
